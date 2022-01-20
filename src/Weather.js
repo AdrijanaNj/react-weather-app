b72cfb78d3e9d844300817ab3bd441d75770b8b2
AdrijanaNj <adrijananjezic@gmail.com>
2022-01-20 20:27:55 +0100

    Changed apiUrl

--- src/Weather.js
+++ src/Weather.js
@@ -22,7 +22,7 @@ export default function Weather(props) {
   function search() {
     const apiKey = "4b3503b2f08a729413c4d33ef1186004";
 
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
   }
 
