5c327ee91922b9df775362f266a41c291eb9341e
AdrijanaNj <adrijananjezic@gmail.com>
2022-01-22 21:15:08 +0100

    Changed css

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -17,7 +17,7 @@ export default function WeatherTemperature(props) {
       <span className="WeatherTemperature">
         <span className="temperature">{Math.round(props.celsius)}</span>
         <span className="unit-C">°C</span>
-        <span className="unit-F active">
+        <span className="unit-F">
           |
           <a href="/" onClick={convertToFahrenheit}>
             °F
