839546fbed1c457461cd7a74e9edb913158c8f93
AdrijanaNj <adrijananjezic@gmail.com>
2022-01-23 21:21:11 +0100

    Created forecast layout

--- src/App.js
+++ src/App.js
@@ -4,8 +4,26 @@ import Weather from "./Weather";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <Weather defaultCity="Paris" />
+      <footer className="mt-5">
+        This project was coded by <span>Adrijana Njezic</span> and is{" "}
+        <a
+          href="https://github.com/AdrijanaNj/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://boring-colden-9e1a1a.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hosted on Netlify
+        </a>
+      </footer>
     </div>
   );
 }
--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -30,31 +30,12 @@ export default function WeatherInfo(props) {
           </div>
           <div className="current col-6">
             <span className="weather-today-image">
-              <WeatherIcon code={props.data.icon} />
+              <WeatherIcon code={props.data.icon} size={75} />
             </span>
             <WeatherTemperature celsius={props.data.temperature} />
           </div>
         </div>
       </div>
-
-      <footer className="mt-5">
-        This project was coded by <span>Adrijana Njezic</span> and is{" "}
-        <a
-          href="https://github.com/AdrijanaNj/react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on GitHub
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://boring-colden-9e1a1a.netlify.app/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          hosted on Netlify
-        </a>
-      </footer>
     </div>
   );
 }
